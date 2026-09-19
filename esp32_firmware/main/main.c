c3063a8329725615d99c15560ad9b5b74c49d351
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-19 18:34:26 -0300

    feat(firmware): added the ambient simulation to main and the proyect as a whole

--- esp32_firmware/main/main.c
+++ esp32_firmware/main/main.c
@@ -1,8 +1,10 @@
-/* Arranque: protocolo UART0 + síntesis de señales por eje */
+/* Arranque: protocolo UART0 + síntesis de señales por eje + ambiente */
 #include "protocol.h"
 #include "sensors.h"
+#include "ambiente.h"
 
 void app_main(void) {
     protocolo_init();
     sensors_init();
+    ambiente_init();
 }
