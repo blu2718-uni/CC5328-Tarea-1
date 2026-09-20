0082f968a839183a5f3aa5c6fc0921b0d3d8488b
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-19 23:25:30 -0300

    feat(firmware): main.c now initializes the commands interface

--- esp32_firmware/main/main.c
+++ esp32_firmware/main/main.c
@@ -1,10 +1,12 @@
-/* Arranque: protocolo UART0 + síntesis de señales por eje + ambiente */
+/* Arranque: protocolo UART0 + síntesis de señales + ambiente + comandos */
 #include "protocol.h"
 #include "sensors.h"
 #include "ambiente.h"
+#include "comandos.h"
 
 void app_main(void) {
     protocolo_init();
     sensors_init();
     ambiente_init();
+    comandos_init();
 }
